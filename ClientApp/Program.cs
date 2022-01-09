001e54b847df53123b9ad8ea0b43f1489dd091eb
milicar99 <milica.radic99@uns.ac.rs>
2022-01-09 18:25:41 +0100

    commitsertifikat

--- ClientApp/Program.cs
+++ ClientApp/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.ServiceModel;
 using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using SecurityManager;
+using System.Security.Principal;
 
 namespace ClientApp
 {
@@ -11,15 +14,22 @@ namespace ClientApp
 	{
 		static void Main(string[] args)
 		{
-           
+            string srvCertCN = "pera";
 
-			NetTcpBinding binding = new NetTcpBinding();
-			string address = "net.tcp://localhost:9999/WCFService";
+            NetTcpBinding binding = new NetTcpBinding();
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
 
-			using (WCFClient proxy = new WCFClient(binding, new EndpointAddress(new Uri(address))))
-			{
-				
-			}
+            X509Certificate2 srvCert = CertManager.GetCertificateFromStorage(StoreName.TrustedPeople, StoreLocation.LocalMachine, srvCertCN);
+            EndpointAddress address = new EndpointAddress(new Uri("net.tcp://localhost:9999/WCFService"),
+                                      new X509CertificateEndpointIdentity(srvCert));
+
+            string cltCertCN = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
+
+            using (WCFClient proxy = new WCFClient(binding, address))
+            {
+
+            }
+
 
 			Console.ReadLine();
 		}
