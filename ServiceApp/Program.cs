8dfe3614418a377cc1368d4f7568a334cee0ae86
milicar99 <milica.radic99@uns.ac.rs>
2022-01-09 18:30:11 +0100

    commitsertifikatserverside

--- ServiceApp/Program.cs
+++ ServiceApp/Program.cs
@@ -7,6 +7,9 @@ using System.ServiceModel.Description;
 using Contracts;
 using SecurityManager;
 using System.IdentityModel.Policy;
+using System.Security.Principal;
+using System.ServiceModel.Security;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ServiceApp
 {
@@ -14,21 +17,34 @@ namespace ServiceApp
 	{
 		static void Main(string[] args)
 		{
-			NetTcpBinding binding = new NetTcpBinding();
-			string address = "net.tcp://localhost:9999/WCFService";
+            string srvCertCN = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
+            Console.WriteLine(srvCertCN);
 
-			ServiceHost host = new ServiceHost(typeof(WCFService));
-			host.AddServiceEndpoint(typeof(IWCFService), binding, address);
 
-			host.Description.Behaviors.Remove(typeof(ServiceDebugBehavior));
-			host.Description.Behaviors.Add(new ServiceDebugBehavior() { IncludeExceptionDetailInFaults = true });
+            NetTcpBinding binding = new NetTcpBinding();
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
 
-            host.Authorization.ServiceAuthorizationManager = new CustomServiceAuthorizationManager();
-           
-            host.Authorization.PrincipalPermissionMode = PrincipalPermissionMode.Custom;
-            List<IAuthorizationPolicy> policies = new List<IAuthorizationPolicy>();
-            policies.Add(new CustomAuthorizationPolicy());
-            host.Authorization.ExternalAuthorizationPolicies = policies.AsReadOnly();
+            string address = "net.tcp://localhost:9999/WCFService";
+            ServiceHost host = new ServiceHost(typeof(WCFService));
+            host.AddServiceEndpoint(typeof(IWCFService), binding, address);
+
+
+
+            host.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.Custom;
+            host.Credentials.ClientCertificate.Authentication.CustomCertificateValidator = new ServerClientCertValidator();
+
+
+
+            host.Credentials.ClientCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
+
+            host.Credentials.ServiceCertificate.Certificate = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, srvCertCN);
+
+            //---------------------------------------------------------------------------------------------------------//
+            host.Description.Behaviors.Remove(typeof(ServiceDebugBehavior));
+            host.Description.Behaviors.Add(new ServiceDebugBehavior() { IncludeExceptionDetailInFaults = true });
+            //---------------------------------------------------------------------------------------------------------//
+
+
 
             // TO DO : podesavanje AutidBehaviour-a
             ServiceSecurityAuditBehavior newAudit = new ServiceSecurityAuditBehavior();
@@ -37,12 +53,16 @@ namespace ServiceApp
 
             host.Description.Behaviors.Remove<ServiceSecurityAuditBehavior>();
             host.Description.Behaviors.Add(newAudit);
-                       
-            host.Open();
-			Console.WriteLine("WCFService is opened. Press <enter> to finish...");
-			Console.ReadLine();
 
-			host.Close();
-		}
+
+
+            host.Open();
+            Console.WriteLine("WCFService is opened. Press <enter> to finish...");
+            Console.ReadLine();
+
+
+
+            host.Close();
+        }
 	}
 }
